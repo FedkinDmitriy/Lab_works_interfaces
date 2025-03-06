af8bb345e8643cc0d5abc57a0cd6fd0ba3114069
FedkinDmitriy <fedkindmitriy@yandex.ru>
2025-03-06 21:26:36 +0300

    почти закончена первая группа для Хика (не срабатывает обработчик)

--- Lab_i2/FormMain.Designer.cs
+++ Lab_i2/FormMain.Designer.cs
@@ -28,22 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            label_time1 = new Label();
+            labelCurrentItem = new Label();
+            buttonRun = new Button();
+            tabPage2 = new TabPage();
+            label1 = new Label();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
             SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(780, 557);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.BackColor = SystemColors.GradientInactiveCaption;
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(label_time1);
+            tabPage1.Controls.Add(labelCurrentItem);
+            tabPage1.Controls.Add(buttonRun);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(772, 529);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Первая группа опытов";
+            // 
+            // label_time1
+            // 
+            label_time1.AutoSize = true;
+            label_time1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label_time1.Location = new Point(692, 241);
+            label_time1.Name = "label_time1";
+            label_time1.Size = new Size(19, 21);
+            label_time1.TabIndex = 3;
+            label_time1.Text = "0";
+            // 
+            // labelCurrentItem
+            // 
+            labelCurrentItem.AutoSize = true;
+            labelCurrentItem.Font = new Font("Segoe UI Semibold", 48F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelCurrentItem.Location = new Point(561, 38);
+            labelCurrentItem.Name = "labelCurrentItem";
+            labelCurrentItem.Size = new Size(73, 86);
+            labelCurrentItem.TabIndex = 2;
+            labelCurrentItem.Text = "0";
+            // 
+            // buttonRun
+            // 
+            buttonRun.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            buttonRun.Location = new Point(561, 400);
+            buttonRun.Name = "buttonRun";
+            buttonRun.Size = new Size(150, 50);
+            buttonRun.TabIndex = 1;
+            buttonRun.TabStop = false;
+            buttonRun.Text = "Run";
+            buttonRun.UseVisualStyleBackColor = true;
+            buttonRun.Click += buttonRun_Click;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(772, 529);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(561, 247);
+            label1.Name = "label1";
+            label1.Size = new Size(72, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Время в мс.";
             // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(780, 557);
-            ControlBox = false;
+            Controls.Add(tabControl1);
             FormBorderStyle = FormBorderStyle.Fixed3D;
-            KeyPreview = true;
             MaximizeBox = false;
             Name = "FormMain";
+            ShowIcon = false;
             StartPosition = FormStartPosition.CenterScreen;
+            Text = "Закон Хика";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Button buttonRun;
+        private Label labelCurrentItem;
+        private Label label_time1;
+        private Label label1;
     }
 }
--- Lab_i2/FormMain.cs
+++ Lab_i2/FormMain.cs
@@ -1,10 +1,177 @@
+using System.Xml;
+
 namespace Lab_i2
 {
     public partial class FormMain : Form
     {
+        const int wPanel = 100, hPanel = 40; //размеры панели 
+        int cX, cY; // дл€ расположени€ курсора по центру
+        Random random = new();
+        Panel[] arrPanel = new Panel[9];
+        int countClick = 0; // дл€ подсчЄта попаданий
+        int quantityPanel = 2; // количество панелей активных (начинаем с двух)
+        int targetNumber = 0; // необходимый элемент дл€ пользовател€
+         
         public FormMain()
         {
             InitializeComponent();
+            this.Load += FormMain_Load;
         }
+
+        private void FormMain_Load(object? sender, EventArgs e)
+        {
+            CreateArr(arrPanel);
+            // ¬ычисл€ем центр окна
+            cX = this.Left + this.Width / 2;
+            cY = this.Top + this.Height / 2;
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(cX, cY);
+
+            if (countClick <= 5)
+            {
+                targetNumber = random.Next(1, quantityPanel + 1);
+                labelCurrentItem.Text = targetNumber.ToString(); // выводит номер панели, которую надо выбрать
+                ShuffleArray(arrPanel, quantityPanel);
+                ShowPanel(arrPanel, quantityPanel);
+            }
+            else
+            {
+                quantityPanel++;
+                targetNumber = random.Next(1, quantityPanel + 1);
+                labelCurrentItem.Text = targetNumber.ToString(); // выводит номер панели, которую надо выбрать
+
+                ShuffleArray(arrPanel, quantityPanel);
+                ShowPanel(arrPanel, quantityPanel);
+
+                countClick = 0;
+            }
+
+            
+            this.ActiveControl = null; // ”бираем фокус с кнопки
+        }
+
+        /// <summary>
+        /// отображает панели
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="quantity"></param>
+        void ShowPanel(Panel[] arr, int quantity)
+        {
+            if (quantity <= arr.Length)
+            {
+                int forPositiveY = 250; // стартовые координаты Y относительно формы
+                int forNegativeY = 300; // стартовые координаты Y относительно формы
+
+                for (int i = 0; i < quantity; i++)
+                {
+                    arr[i].Visible = true;
+
+                    // ѕеремещаем панели в новое положение
+                    if (i % 2 == 0)
+                    {
+                        arr[i].Location = new Point(150, forPositiveY);
+                        forPositiveY -= 50;
+                    }
+                    else
+                    {
+                        arr[i].Location = new Point(150, forNegativeY);
+                        forNegativeY += 50;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// заполн€ет массив и размещает его
+        /// </summary>
+        /// <param name="arr"></param>
+        void CreateArr(Panel[] arr)
+        {
+            int forPositiveY = 250; // стартовые координаты Y относительно формы
+            int forNegativeY = 300; // стартовые координаты Y относительно формы
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new Panel
+                {
+                    Size = new Size(wPanel, hPanel),
+                    BackColor = Color.White,
+                    Visible = false
+                };
+                Label label = new Label
+                {
+                    Text = (i + 1).ToString(),
+                    ForeColor = Color.Blue,
+                    Font = new Font("Times New Roman", 14, FontStyle.Bold),
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Dock = DockStyle.Fill
+                };
+                arr[i].Controls.Add(label); // ƒобавл€ем метку в панель
+                tabPage1.Controls.Add(arr[i]); // ƒобавл€ем панель в tabPage1
+
+                arr[i].Click += Panel_Click; // прив€жем обработчик к каждой панели
+                
+
+                //System.Diagnostics.Debug.WriteLine($"ќбработчик прив€зан к панели {i + 1}");
+
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    arr[i].Location = new Point(150, forPositiveY);
+                    forPositiveY -= 50;
+                }
+                else
+                {
+                    arr[i].Location = new Point(150, forNegativeY);
+                    forNegativeY += 50;
+                }
+            }
+        }
+
+        private void Panel_Click(object? sender, EventArgs e)
+        {
+            //System.Diagnostics.Debug.WriteLine("—работал Panel_Click");
+
+            if (sender is Panel panel)
+            {
+                if (panel.Controls[0] is Label label)
+                {
+                    int num = Convert.ToInt32(label.Text);
+                    if(num == targetNumber)
+                    {
+                        countClick++;
+                    }
+                }
+            }
+
+            //label_time1.Text = countClick.ToString(); 
+        }
+
+        /// <summary>
+        /// перемешивает массив
+        /// </summary>
+        /// <param name="array"></param>
+        private void ShuffleArray(Panel[] array, int currentBorder)
+        {
+            Random rand = new Random();
+            int n = currentBorder;
+            while (n > 1)
+            {
+                n--;  // ”меньшаем размер массива на 1, чтобы переставить элементы с оставшимис€
+                int k = rand.Next(n + 1);  // ѕолучаем случайное число от 0 до n (включительно)
+
+                // ѕеремещаем элементы массива
+                Panel temp = array[n];  // —охран€ем текущий элемент в temp
+                array[n] = array[k];  // ѕеремещаем случайный элемент в текущее место
+                array[k] = temp;  // ѕеремещаем элемент из temp в случайное место
+            }
+        }
+
+
     }
 }
