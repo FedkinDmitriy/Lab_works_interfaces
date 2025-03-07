22570d557ead86f5087bfbca32f9cfb310dedfe8
FedkinDmitriy <fedkindmitriy@yandex.ru>
2025-03-07 17:57:54 +0300

    закончил вторую группу. осталось собрать тестовые данные

--- Lab_i2/FormMain.Designer.cs
+++ Lab_i2/FormMain.Designer.cs
@@ -35,8 +35,23 @@
             labelCurrentItem = new Label();
             buttonRun = new Button();
             tabPage2 = new TabPage();
+            button_reset = new Button();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            radioButton_back = new RadioButton();
+            radioButton_font = new RadioButton();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            radioButton_black = new RadioButton();
+            radioButton_red = new RadioButton();
+            radioButton_green = new RadioButton();
+            radioButton_yellow = new RadioButton();
+            label_time2 = new Label();
+            label2 = new Label();
+            buttonRun2 = new Button();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            flowLayoutPanel2.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // tabControl1
@@ -67,9 +82,10 @@
             // label1
             // 
             label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
             label1.Location = new Point(539, 246);
             label1.Name = "label1";
-            label1.Size = new Size(72, 15);
+            label1.Size = new Size(101, 21);
             label1.TabIndex = 4;
             label1.Text = "Время в мс.";
             // 
@@ -77,7 +93,7 @@
             // 
             label_time1.AutoSize = true;
             label_time1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            label_time1.Location = new Point(670, 240);
+            label_time1.Location = new Point(670, 246);
             label_time1.Name = "label_time1";
             label_time1.Size = new Size(19, 21);
             label_time1.TabIndex = 3;
@@ -107,13 +123,146 @@
             // 
             // tabPage2
             // 
+            tabPage2.BackColor = SystemColors.GradientInactiveCaption;
+            tabPage2.Controls.Add(button_reset);
+            tabPage2.Controls.Add(flowLayoutPanel2);
+            tabPage2.Controls.Add(flowLayoutPanel1);
+            tabPage2.Controls.Add(label_time2);
+            tabPage2.Controls.Add(label2);
+            tabPage2.Controls.Add(buttonRun2);
             tabPage2.Location = new Point(4, 24);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
             tabPage2.Size = new Size(772, 529);
             tabPage2.TabIndex = 1;
-            tabPage2.Text = "tabPage2";
-            tabPage2.UseVisualStyleBackColor = true;
+            tabPage2.Text = "Вторая группа опытов";
+            // 
+            // button_reset
+            // 
+            button_reset.Location = new Point(632, 137);
+            button_reset.Name = "button_reset";
+            button_reset.Size = new Size(132, 50);
+            button_reset.TabIndex = 5;
+            button_reset.Text = "сброс";
+            button_reset.UseVisualStyleBackColor = true;
+            button_reset.Click += button_reset_Click;
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.Controls.Add(radioButton_back);
+            flowLayoutPanel2.Controls.Add(radioButton_font);
+            flowLayoutPanel2.Location = new Point(632, 21);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(132, 110);
+            flowLayoutPanel2.TabIndex = 4;
+            // 
+            // radioButton_back
+            // 
+            radioButton_back.AutoSize = true;
+            radioButton_back.Location = new Point(3, 3);
+            radioButton_back.Name = "radioButton_back";
+            radioButton_back.Size = new Size(66, 19);
+            radioButton_back.TabIndex = 0;
+            radioButton_back.TabStop = true;
+            radioButton_back.Text = "Панель";
+            radioButton_back.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_font
+            // 
+            radioButton_font.AutoSize = true;
+            radioButton_font.Location = new Point(3, 28);
+            radioButton_font.Name = "radioButton_font";
+            radioButton_font.Size = new Size(64, 19);
+            radioButton_font.TabIndex = 1;
+            radioButton_font.TabStop = true;
+            radioButton_font.Text = "Шрифт";
+            radioButton_font.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(radioButton_black);
+            flowLayoutPanel1.Controls.Add(radioButton_red);
+            flowLayoutPanel1.Controls.Add(radioButton_green);
+            flowLayoutPanel1.Controls.Add(radioButton_yellow);
+            flowLayoutPanel1.Location = new Point(494, 21);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(132, 110);
+            flowLayoutPanel1.TabIndex = 3;
+            // 
+            // radioButton_black
+            // 
+            radioButton_black.AutoSize = true;
+            radioButton_black.Location = new Point(3, 3);
+            radioButton_black.Name = "radioButton_black";
+            radioButton_black.Size = new Size(69, 19);
+            radioButton_black.TabIndex = 0;
+            radioButton_black.TabStop = true;
+            radioButton_black.Text = "Чёрный";
+            radioButton_black.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_red
+            // 
+            radioButton_red.AutoSize = true;
+            radioButton_red.Location = new Point(3, 28);
+            radioButton_red.Name = "radioButton_red";
+            radioButton_red.Size = new Size(74, 19);
+            radioButton_red.TabIndex = 1;
+            radioButton_red.TabStop = true;
+            radioButton_red.Text = "Красный";
+            radioButton_red.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_green
+            // 
+            radioButton_green.AutoSize = true;
+            radioButton_green.Location = new Point(3, 53);
+            radioButton_green.Name = "radioButton_green";
+            radioButton_green.Size = new Size(74, 19);
+            radioButton_green.TabIndex = 2;
+            radioButton_green.TabStop = true;
+            radioButton_green.Text = "Зелёный";
+            radioButton_green.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_yellow
+            // 
+            radioButton_yellow.AutoSize = true;
+            radioButton_yellow.Location = new Point(3, 78);
+            radioButton_yellow.Name = "radioButton_yellow";
+            radioButton_yellow.Size = new Size(70, 19);
+            radioButton_yellow.TabIndex = 3;
+            radioButton_yellow.TabStop = true;
+            radioButton_yellow.Text = "Жёлтый";
+            radioButton_yellow.UseVisualStyleBackColor = true;
+            // 
+            // label_time2
+            // 
+            label_time2.AutoSize = true;
+            label_time2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label_time2.Location = new Point(693, 335);
+            label_time2.Name = "label_time2";
+            label_time2.Size = new Size(19, 21);
+            label_time2.TabIndex = 2;
+            label_time2.Text = "0";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.Location = new Point(562, 335);
+            label2.Name = "label2";
+            label2.Size = new Size(101, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Время в мс.";
+            // 
+            // buttonRun2
+            // 
+            buttonRun2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            buttonRun2.Location = new Point(562, 393);
+            buttonRun2.Name = "buttonRun2";
+            buttonRun2.Size = new Size(150, 50);
+            buttonRun2.TabIndex = 0;
+            buttonRun2.Text = "Run";
+            buttonRun2.UseVisualStyleBackColor = true;
+            buttonRun2.Click += buttonRun2_Click;
             // 
             // FormMain
             // 
@@ -130,6 +279,12 @@
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
             tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            flowLayoutPanel2.ResumeLayout(false);
+            flowLayoutPanel2.PerformLayout();
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -142,5 +297,17 @@
         private Label labelCurrentItem;
         private Label label_time1;
         private Label label1;
+        private Button buttonRun2;
+        private Label label_time2;
+        private Label label2;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private RadioButton radioButton_black;
+        private RadioButton radioButton_red;
+        private RadioButton radioButton_green;
+        private RadioButton radioButton_yellow;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private RadioButton radioButton_back;
+        private RadioButton radioButton_font;
+        private Button button_reset;
     }
 }
--- Lab_i2/FormMain.cs
+++ Lab_i2/FormMain.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
+using System.Drawing;
 using System.Xml;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace Lab_i2
 {
@@ -18,19 +20,36 @@ namespace Lab_i2
         private readonly string txtFilePath = "results.txt";
         private readonly string csvFilePath = "results.csv";
 
+
+        //****************************2 группа*************************//
+        Color[] colors = [Color.Black, Color.Yellow, Color.Green, Color.Red]; // массив цветов, исключая заданный цвет шрифта (синий) и панели (белый)
+        Panel[] arrPanel2 = new Panel[9]; // для второй группы
+        bool isSelected2 = false;
+        int countClick2 = 0; // для подсчёта попаданий для второй группы
+        int quantityPanel2 = 2; // количество панелей активных (начинаем с двух) для второй группы
+        List<double> averageTime2 = new(); //для записи среднего для каждой группы объектов для второй группы
+        Stopwatch sw2 = new(); //секундомер для второй группы
+        private readonly string txtFilePath2 = "results2.txt";
+        private readonly string csvFilePath2 = "results2.csv";
+
+        Color currentColorBack = Color.White, currentColorFont = Color.Blue;
+
         public FormMain()
         {
             InitializeComponent();
             this.Load += FormMain_Load;
+            radioButton_back.Checked = true;
+            radioButton_black.Checked = true;
         }
 
         private void FormMain_Load(object? sender, EventArgs e)
         {
             CreateArr(arrPanel);
+            CreateArr2(arrPanel2); // создаем во второй вкладке независимый массив с панелями
             // Вычисляем центр окна
             cX = this.Left + this.Width / 2;
             cY = this.Top + this.Height / 2;
-           
+
         }
 
         private void buttonRun_Click(object sender, EventArgs e)
@@ -68,13 +87,13 @@ namespace Lab_i2
                     {
                         arrPanel[i].Visible = false;
                     }
-                    MessageBox.Show("первая группа экспериментов звершена", "!", MessageBoxButtons.OK, MessageBoxIcon.Information );
+                    MessageBox.Show("первая группа экспериментов звершена", "!", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     CreateArr(arrPanel);
                 }
             }
 
             sw.Start();
-            
+
             this.ActiveControl = null; // Убираем фокус с кнопки
         }
 
@@ -176,10 +195,10 @@ namespace Lab_i2
                             double res = sw.Elapsed.TotalMilliseconds;
                             label_time1.Text = res.ToString("F2");
                             averageTime.Add(res);
-                            if(averageTime.Count >= 5)
+                            if (averageTime.Count >= 5)
                             {
                                 double avg = averageTime.Average();
-                                SaveResultsToFile(Math.Round(avg, 2), quantityPanel);
+                                SaveResultsToFile(Math.Round(avg, 2), quantityPanel, txtFilePath, csvFilePath);
                                 averageTime.Clear();
                             }
                             sw.Reset();
@@ -191,8 +210,8 @@ namespace Lab_i2
                     }
                 }
             }
-          
-            
+
+
         }
 
         /// <summary>
@@ -218,17 +237,335 @@ namespace Lab_i2
         /// <summary>
         /// Записывает результаты эксперимента в два файла (txt и csv).
         /// </summary>
-        private void SaveResultsToFile(double avgTime, int quantity)
+        private void SaveResultsToFile(double avgTime, int quantity, string txtPath, string csvPath)
         {
             try
             {
-                using (StreamWriter writer = File.AppendText(txtFilePath))
+                using (StreamWriter writer = File.AppendText(txtPath))
                 {
                     writer.WriteLine($"Количество объектов: {quantityPanel} среднее время из 5 попыток: {avgTime}");
                 }
-                using (StreamWriter writer = File.AppendText(csvFilePath))
+                using (StreamWriter writer = File.AppendText(csvPath))
                 {
                     writer.WriteLine($"{quantityPanel};{avgTime}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка записи в файл: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+
+        //*************обработчики и методы для 2й группы*******************//
+
+        private void buttonRun2_Click(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(cX, cY);
+            isSelected2 = false;
+
+
+            if (radioButton_back.Checked)
+            {
+                ResetBack(arrPanel2, quantityPanel2);
+                currentColorFont = Color.Blue;
+
+                if (radioButton_black.Checked)
+                {
+                    RandBack(arrPanel2, quantityPanel2, Color.Black);
+                    currentColorBack = Color.Black;
+                }
+                else if (radioButton_red.Checked)
+                {
+                    RandBack(arrPanel2, quantityPanel2, Color.Red);
+                    currentColorBack = Color.Red;
+                }
+                else if (radioButton_green.Checked)
+                {
+                    RandBack(arrPanel2, quantityPanel2, Color.Green);
+                    currentColorBack = Color.Green;
+                }
+                else if (radioButton_yellow.Checked)
+                {
+                    RandBack(arrPanel2, quantityPanel2, Color.Yellow);
+                    currentColorBack = Color.Yellow;
+                }
+            }
+            else if (radioButton_font.Checked)
+            {
+                ResetColorFont(arrPanel2, quantityPanel2);
+                currentColorBack = Color.White;
+
+                if (radioButton_black.Checked)
+                {
+                    RandColorFont(arrPanel2, quantityPanel2, Color.Black);
+                    currentColorFont = Color.Black;
+                }
+                else if (radioButton_red.Checked)
+                {
+                    RandColorFont(arrPanel2, quantityPanel2, Color.Red);
+                    currentColorFont = Color.Red;
+                }
+                else if (radioButton_green.Checked)
+                {
+                    RandColorFont(arrPanel2, quantityPanel2, Color.Green);
+                    currentColorFont = Color.Green;
+                }
+                else if (radioButton_yellow.Checked)
+                {
+                    RandColorFont(arrPanel2, quantityPanel2, Color.Yellow);
+                    currentColorFont = Color.Yellow;
+                }
+            }
+
+
+            if (countClick2 < 5)
+            {
+                ShowPanel2(arrPanel2, quantityPanel2);
+            }
+            else
+            {
+                if (quantityPanel2 < arrPanel2.Length)
+                {
+                    quantityPanel2++;
+                    countClick2 = 0;
+                    ShowPanel2(arrPanel2, quantityPanel2);
+                }
+                else
+                {
+                    quantityPanel2 = 2;
+                    countClick2 = 0;
+
+                    for (int i = 0; i < arrPanel2.Length; i++)
+                    {
+                        arrPanel2[i].Visible = false;
+                    }
+                    MessageBox.Show("вторая группа экспериментов звершена", "!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    CreateArr2(arrPanel2);
+                }
+            }
+
+            sw2.Start();
+
+            this.ActiveControl = null; // Убираем фокус с кнопки
+        }
+
+        private void Panel_Click2(object? sender, EventArgs e)
+        {
+            if (radioButton_back.Checked)
+            {
+                if (sender is Panel panel)
+                {
+                    if (radioButton_black.Checked && !isSelected2 && panel.BackColor == Color.Black)
+                    {
+                        isSelected2 = true;
+                        countClick2++;
+
+                    }
+                    else if (radioButton_red.Checked && !isSelected2 && panel.BackColor == Color.Red)
+                    {
+                        isSelected2 = true;
+                        countClick2++;
+                    }
+                    else if (radioButton_green.Checked && !isSelected2 && panel.BackColor == Color.Green)
+                    {
+                        isSelected2 = true;
+                        countClick2++;
+                    }
+                    else if (radioButton_yellow.Checked && !isSelected2 && panel.BackColor == Color.Yellow)
+                    {
+                        isSelected2 = true;
+                        countClick2++;
+                    }
+
+                    Cursor.Position = new Point(cX, cY);
+                    sw2.Stop();
+                    double res = sw2.Elapsed.TotalMilliseconds;
+
+                    label_time2.Text = res.ToString("F2");
+
+                    averageTime2.Add(res);
+                    if (averageTime2.Count >= 5)
+                    {
+                        double avg = averageTime2.Average();
+                        SaveResultsToFile2(Math.Round(avg, 2), quantityPanel2, txtFilePath2, csvFilePath2);
+                        averageTime2.Clear();
+                    }
+                    sw2.Reset();
+
+                }
+
+            }
+            else if (radioButton_font.Checked)
+            {
+                if(sender is Panel panel)
+                {
+                    if (panel.Controls[0] is Label label)
+                    {
+                        if (radioButton_black.Checked && !isSelected2 && label.ForeColor == Color.Black)
+                        {
+                            isSelected2 = true;
+                            countClick2++;
+
+                        }
+                        else if (radioButton_red.Checked && !isSelected2 && label.ForeColor == Color.Red)
+                        {
+                            isSelected2 = true;
+                            countClick2++;
+                        }
+                        else if (radioButton_green.Checked && !isSelected2 && label.ForeColor == Color.Green)
+                        {
+                            isSelected2 = true;
+                            countClick2++;
+                        }
+                        else if (radioButton_yellow.Checked && !isSelected2 && label.ForeColor == Color.Yellow)
+                        {
+                            isSelected2 = true;
+                            countClick2++;
+                        }
+
+                        Cursor.Position = new Point(cX, cY);
+                        sw2.Stop();
+                        double res = sw2.Elapsed.TotalMilliseconds;
+
+                        label_time2.Text = res.ToString("F2");
+
+                        averageTime2.Add(res);
+                        if (averageTime2.Count >= 5)
+                        {
+                            double avg = averageTime2.Average();
+                            SaveResultsToFile2(Math.Round(avg, 2), quantityPanel2, txtFilePath2, csvFilePath2);
+                            averageTime2.Clear();
+                        }
+                        sw2.Reset();
+
+                    }
+                }
+            }
+        }
+
+        void ShowPanel2(Panel[] arr, int quantity)
+        {
+            if (quantity <= arr.Length)
+            {
+                int forPositiveY = 250; // стартовые координаты Y относительно формы
+                int forNegativeY = 300; // стартовые координаты Y относительно формы
+
+                for (int i = 0; i < quantity; i++)
+                {
+                    arr[i].Visible = true;
+
+                    // Перемещаем панели в новое положение
+                    if (i % 2 == 0)
+                    {
+                        arr[i].Location = new Point(150, forPositiveY);
+                        forPositiveY -= 50;
+                    }
+                    else
+                    {
+                        arr[i].Location = new Point(150, forNegativeY);
+                        forNegativeY += 50;
+                    }
+                }
+            }
+        }
+
+        void CreateArr2(Panel[] arr)
+        {
+            int forPositiveY = 250; // стартовые координаты Y относительно формы
+            int forNegativeY = 300; // стартовые координаты Y относительно формы
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new Panel
+                {
+                    Size = new Size(wPanel, hPanel),
+                    BackColor = Color.White,
+                    Visible = false
+                };
+                Label label = new Label
+                {
+                    Text = (i + 1).ToString(),
+                    ForeColor = Color.Blue,
+                    Font = new Font("Times New Roman", 14, FontStyle.Bold),
+                    TextAlign = ContentAlignment.MiddleLeft,
+                    AutoSize = true
+                };
+                arr[i].Controls.Add(label); // Добавляем метку в панель
+                tabPage2.Controls.Add(arr[i]); // Добавляем панель в tabPage2
+                label.Location = new Point(20, (arr[i].Height - label.Height) / 2); // Смещение от левого края и по центру
+                arr[i].Click += Panel_Click2; // привяжем обработчик к каждой панели
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    arr[i].Location = new Point(150, forPositiveY);
+                    forPositiveY -= 50;
+                }
+                else
+                {
+                    arr[i].Location = new Point(150, forNegativeY);
+                    forNegativeY += 50;
+                }
+            }
+        }
+
+        void RandBack(Panel[] arr, int border, Color color)
+        {
+            Random rand = new();
+            arr[rand.Next(border)].BackColor = color;
+
+        }
+        void ResetBack(Panel[] arr, int border)
+        {
+            for (int i = 0; i < border; i++)
+            {
+                arr[i].BackColor = Color.White;
+            }
+        }
+
+        void RandColorFont(Panel[] arr, int border, Color color)
+        {
+            Random rand = new();
+            arr[rand.Next(border)].Controls[0].ForeColor = color;
+        }
+        void ResetColorFont(Panel[] arr, int border)
+        {
+            for (int i = 0; i < border; i++)
+            {
+                arr[i].Controls[0].ForeColor = Color.Blue;
+            }
+        }
+
+        private void button_reset_Click(object sender, EventArgs e)
+        {
+            countClick2 = 0;
+            quantityPanel2 = 2;
+            isSelected2 = false;
+
+            foreach (var panel in arrPanel2)
+            {
+                tabPage2.Controls.Remove(panel);
+            }
+
+            CreateArr2(arrPanel2);
+        }
+
+
+        private void SaveResultsToFile2(double avgTime, int quantity, string txtPath, string csvPath)
+        {
+            try
+            {
+                using (StreamWriter writer = File.AppendText(txtPath))
+                {
+                    writer.WriteLine($"Количество объектов: {quantityPanel2} цвет панели: {currentColorBack} цвет шрифта: {currentColorFont} среднее время из 5 попыток: {avgTime}");
+                }
+                using (StreamWriter writer = File.AppendText(csvPath))
+                {
+                    writer.WriteLine($"{quantityPanel2};{currentColorBack};{currentColorFont};{avgTime}");
                 }
             }
             catch (Exception ex)
